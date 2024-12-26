ad2b800423914c5153b08d6fa1e458a691bcd550
haeseonkim <khs51777@naver.com>
2024-12-26 21:07:15 +0900

    init project

---
+++ src/main/java/com/abab/lecture/LectureBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abab.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abab/lecture/LectureBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abab.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
