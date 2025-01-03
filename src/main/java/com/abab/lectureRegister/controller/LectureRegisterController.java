1edf0490c32578232054dbd0c2c164659343c0b8
haeseonkim <khs51777@naver.com>
2025-01-03 22:15:47 +0900

    :recycle: refactor: api를 RESTful하게 수정

--- src/main/java/com/abab/lectureRegister/controller/LectureRegisterController.java
+++ src/main/java/com/abab/lectureRegister/controller/LectureRegisterController.java
@@ -1,9 +1,8 @@
 package com.abab.lectureRegister.controller;
 
+import com.abab.lectureRegister.dto.RegisterLectureRequest;
 import com.abab.lectureRegister.service.LectureRegisterServiceFacade;
-import com.abab.lectureRegister.model.Lecture;
 import com.abab.lectureRegister.dto.LectureResponse;
-import com.abab.lectureRegister.model.Registration;
 import com.abab.lectureRegister.dto.RegistrationResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -12,43 +11,33 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/lecture")
+@RequestMapping("/lectures")
 public class LectureRegisterController {
     private final LectureRegisterServiceFacade lectureRegisterServiceFacade;
 
-    @GetMapping("/date")
+    @GetMapping
     public ResponseEntity<List<LectureResponse>> getDateLecture(
             @RequestParam(required = false) LocalDateTime startDateTime
     ) {
-        List<Lecture> lectures = lectureRegisterServiceFacade.getLectures(startDateTime);
-        List<LectureResponse> response = lectures.stream()
-                .map(LectureResponse::new)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(lectureRegisterServiceFacade.getLectures(startDateTime));
     }
 
-    @PostMapping("/{lectureId}/register")
+    @PostMapping("/{lectureId}/registrations")
     public ResponseEntity<RegistrationResponse> registerLecture(
             @PathVariable Long lectureId,
-            @RequestParam Long userId
+            @RequestParam RegisterLectureRequest registerLectureRequest
     ) {
-        Registration registration = lectureRegisterServiceFacade.registerLecture(userId, lectureId);
-        return ResponseEntity.ok(new RegistrationResponse(registration));
+        return ResponseEntity.ok(lectureRegisterServiceFacade.registerLecture(registerLectureRequest.getUserId(), lectureId));
     }
 
-    @GetMapping("/registered")
+    @GetMapping("/users/{userId}/registrations")
     public ResponseEntity<List<LectureResponse>> getRegisteredLectures(
-            @RequestParam Long userId
+            @PathVariable Long userId
     ) {
-        List<Lecture> lectures = lectureRegisterServiceFacade.getRegisteredLectures(userId);
-        List<LectureResponse> response = lectures.stream()
-                .map(LectureResponse::new)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(lectureRegisterServiceFacade.getRegisteredLectures(userId));
     }
 
 }
